c5b8ce35bd5f14d33348c0a1151dc8174883528b
Anthony Shkurenko <tonyshkurenko@gmail.com>
2016-05-11 14:06:24 +0300

    Initial commit

---
+++ app/src/main/java/io/github/tonyshkurenko/letssmoke/SmokeActivity.java
@@ -0,0 +1,46 @@
+package io.github.tonyshkurenko.letssmoke;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SmokeActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+  @BindView(R.id.cigarette) View mCigarette;
+  @BindView(R.id.seek_bar) SeekBar mSeekBar;
+
+  private int mDefaultHeight;
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_smoke);
+    ButterKnife.bind(this);
+
+    mDefaultHeight = (int) (getResources().getDimension(R.dimen.default_cigarette_height));
+
+    mSeekBar.setOnSeekBarChangeListener(this);
+  }
+
+  @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+    final ViewGroup.LayoutParams params = mCigarette.getLayoutParams();
+
+    params.height =
+        mDefaultHeight + (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, progress,
+            getResources().getDisplayMetrics());
+
+    mCigarette.setLayoutParams(params);
+  }
+
+  @Override public void onStartTrackingTouch(SeekBar seekBar) {
+
+  }
+
+  @Override public void onStopTrackingTouch(SeekBar seekBar) {
+
+  }
+}
